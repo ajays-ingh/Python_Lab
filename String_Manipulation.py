809d0a8a431e1d54fae21eff5e4c3f41b7d27baa
ajays-ingh <ajayinghrajput@gmail.com>
2026-09-14 15:28:06 +0530

    Refactor string manipulation code for clarity

--- String_Manipulation.py
+++ String_Manipulation.py
@@ -1,11 +1,14 @@
-s=input("Enter any string: ")
+s = input("Enter any string: ")  # Take input
 
-print("Uppercase",s.upper())
-print("Lower",s.lower())
-print("Reversed",s[::-1])
+print("Uppercase", s.upper())  # Convert to uppercase
+print("Lower", s.lower())  # Convert to lowercase
+print("Reversed", s[::-1])  # Reverse the string
 
-count = 0 
-for char in s.lower():
-    if char in 'aeiou':
-        count+=1
-print("Number of vowels",count)    
+count = 0  # Set count to 0
+
+for char in s.lower():  # Check each character
+    if char in 'aeiou':  # Check for vowel
+        count += 1
+
+print("Number of vowels", count)  # Print vowel count
+   
