487a6c5fdec620792c3dad71b40dc66a9661a71e
ajays-ingh <ajayinghrajput@gmail.com>
2026-09-14 15:29:39 +0530

    Add comments to List.py for clarity
    Added comments to clarify list operations.

--- List.py
+++ List.py
@@ -1,4 +1,7 @@
-fruits=["Apple","Banana","Grapes","Pineapple","Peach"]
-print(fruits[1],fruits[3])
-fruits[-1]="Mango"
-print(fruits)
+fruits = ["Apple", "Banana", "Grapes", "Pineapple", "Peach"]  # Create list
+
+print(fruits[1], fruits[3])  # Print 2nd and 4th items
+
+fruits[-1] = "Mango"  # Change last item
+
+print(fruits)  # Print updated list
